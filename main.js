40939e86e3f4720ecdd54dd50a4c47106dd93ef4
wporta06 <portawalid@gmail.com>
2021-09-20 20:45:42 +0100

    v2 add clock

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ const inputnote = document.querySelector("#inputnote");
 const form = document.querySelector("#form");
 const container = document.querySelector(".container");
 
+// nave bare
+// when hover change color bottun
+const changecolor = document.querySelector("#changecolor");
+changecolor.addEventListener("mouseover", (eo) => {
+    hidenul.style.display = "block";
+})
+changecolor.addEventListener("mouseout", (eo) => {
+    hidenul.style.display = "none";
+})
+const bavebg = document.querySelector("#bavebg");
+const myheader = document.querySelector(".myheader");
+
+bavebg.addEventListener("click", (eo) => {
+        const random = Math.round(Math.random() * 100)
+        myheader.style.backgroundColor = `hsl(${random},44%,55%)`;
+    })
+    // console.log(myheader);
+
+
 
 // add tasks
 form.addEventListener("submit", (eo) => { //or using inputnote and click event
@@ -55,5 +74,34 @@ container.addEventListener('click', (eo) => {
         container.prepend(eo.target.parentNode);
 
     }
-    // console.log("dd");
 })
+
+// time
+const houre = document.querySelector("#houre");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+const TTT = () => {
+    let date = new Date();
+
+    // houre.innerHTML = date.getHours();
+    // min.innerHTML = date.getMinutes();
+    // sec.innerHTML = date.getSeconds();
+
+    if (date.getHours() < 10) {
+        houre.innerHTML = `0${date.getHours()}`;
+    } else {
+        houre.innerHTML = `${date.getHours()}`;
+    }
+    if (date.getMinutes() < 10) {
+        min.innerHTML = `0${date.getMinutes()}`;
+    } else {
+        min.innerHTML = `${date.getMinutes()}`;
+    }
+    if (date.getSeconds() < 10) {
+        sec.innerHTML = `0${date.getSeconds()}`;
+    } else {
+        sec.innerHTML = `${date.getSeconds()}`;
+    }
+}
+
+setInterval(TTT, 1000)
